55274d4ad5163a0f5e14a793db62f030d5b2066c
viraj <virajsutar1539@gmail.com>
2025-07-30 20:20:36 +0530

    updated design

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import './App.css'  // Optional if you use Tailwind
 
 const queryClient = new QueryClient();
 
